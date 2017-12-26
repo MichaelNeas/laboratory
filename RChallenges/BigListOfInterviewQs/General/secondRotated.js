4007bda4b012467dff92cc8337ba9142f201eb65
Michael Neas <michael.r.neas@gmail.com>
2017-12-25 22:48:28 -0500

    Nlogn alg for checking second array is rotated

--- RChallenges/BigListOfInterviewQs/General/secondRotated.js
+++ RChallenges/BigListOfInterviewQs/General/secondRotated.js
@@ -31,22 +31,41 @@ const dataObj = {
     };
     console.log(isReversed(dataObj.data, dataObj.data1));
     console.log(isReversed(dataObj.data, dataObj.data2));
-    //tho the sum should be the same that doesn't mean they're equal
+    //O(n^2) tho the sum should be the same that doesn't mean they're equal
     let isARotatedBRUTE = (arr1, arr2) => {
         if(arr1.length === arr2.length){
+            //make a copy of arr2
+            let temp = [...arr2];
             arr1.forEach(element => {
                 let i;
                 for(i=0;i<arr1.length;i++){
-                    if(element === arr2[i]){
+                    if(element === temp[i]){
                         //console.log(element, ": found! ", arr2)
-                        arr2.splice(i,1);
+                        temp.splice(i,1);
                     }
                 }
             });
-            return arr2.length>0 ? false : true;
+            return temp.length>0 ? false : true;
         }
         return false;
     };
     console.log(isARotatedBRUTE(dataObj.A, dataObj.B));
     console.log(isARotatedBRUTE(dataObj.A, dataObj.C));
+
+    //nlogn
+    let sortedRotation = (arr1, arr2) => {
+        if(arr1.length === arr2.length){
+            arr1.sort();
+            arr2.sort();
+            let i;
+            for(i=0;i<arr1.length; i++){
+                if(arr1[i] !== arr2[i])
+                    return false;
+            }
+            return true;
+        }
+        return false;
+    }
+    console.log(sortedRotation(dataObj.A, dataObj.B));
+    console.log(sortedRotation(dataObj.A, dataObj.C));
 })(dataObj);
