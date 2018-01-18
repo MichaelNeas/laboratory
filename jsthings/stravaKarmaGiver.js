f4c2f569b292ce33d432190b23031445372c43f7
Michael Neas <michael.r.neas@gmail.com>
2018-01-17 22:39:55 -0500

    the real method i meant to use

--- jsthings/stravaKarmaGiver.js
+++ jsthings/stravaKarmaGiver.js
@@ -1,10 +1,7 @@
-//scrape site and give kudos to everyone
-var counter = 0;
-while(counter < 10){
+//woops
+setInterval(function(){
+    window.scrollTo(0,document.body.scrollHeight);
     var elems = document.querySelectorAll('[title="Give Kudos"]');
     for(var z=0;z<elems.length;z++)
         elems[z].click();
-    window.scrollTo(0,document.body.scrollHeight);
-    console.log(counter)
-    setTimeout(counter++,3000); //meh
-}
+},6000)
