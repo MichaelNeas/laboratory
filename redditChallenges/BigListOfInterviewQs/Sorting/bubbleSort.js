6820eeaf48fc08a654cee10abcdcabd1bb564359
Michael Neas <michael.r.neas@gmail.com>
2018-01-21 21:26:51 -0500

    added some bubbly optimizations and stopped mutating state

--- redditChallenges/BigListOfInterviewQs/Sorting/bubbleSort.js
+++ redditChallenges/BigListOfInterviewQs/Sorting/bubbleSort.js
@@ -3,6 +3,7 @@
 let data = [3, 7, 2, 4, 1, 9, 8, 5];
 
 let bubbleSort = arr => {
+    arr = [...arr]
     let swapped = true;
     while(swapped){
         let i;
@@ -17,6 +18,30 @@ let bubbleSort = arr => {
             console.log(arr)
         }
     }
+    return arr;
+}
+
+let optimizedBubbs = arr => {
+    arr = [...arr]
+    let swapped = true;
+    let checkBoundary = arr.length - 1;
+    while(swapped){
+        swapped = false;
+        let i;
+        for(i = 0; i < checkBoundary ; i++){
+            if(arr[i] > arr[i+1]){
+                let temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                swapped = true;
+            }
+            console.log(arr)
+        }
+        checkBoundary--;
+    }
+    return arr;
 }
 
 bubbleSort(data);
+console.log('------------optimizations------------');
+optimizedBubbs(data);
