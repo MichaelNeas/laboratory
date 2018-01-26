9d448749e86b46e417ad10fb817ea9ffce4c9654
Michael Neas <michael.r.neas@gmail.com>
2018-01-25 22:04:08 -0500

    mdn has great docs for generators

--- jsthings/generator.js
+++ jsthings/generator.js
@@ -48,3 +48,16 @@ function runGenerator(generator){
 				.then(resp => iterator.next(resp))
 		})
 }
+
+/**mdn generators */
+function* idMaker() {
+	var index = 0;
+	while(true)
+		yield index++;
+}
+
+const gen = idMaker();
+
+console.log(gen.next().value); // 0
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 2
