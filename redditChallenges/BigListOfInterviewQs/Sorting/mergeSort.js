7bdd9cc0563fafb51fe8779f303ab881ad0d0ee4
Michael Neas <michael.r.neas@gmail.com>
2018-01-28 21:03:47 -0500

    Much better merge sort #1

--- redditChallenges/BigListOfInterviewQs/Sorting/mergeSort.js
+++ redditChallenges/BigListOfInterviewQs/Sorting/mergeSort.js
@@ -5,14 +5,22 @@ let data = [20, 12, 10, 15, 2, 64, 25, 12, 22, 11];
 let merge = (oneArray, anotherArray) => {
     let mergedArray = [];
     while(oneArray.length > 0 || anotherArray.length > 0){
-        if(oneArray.length === 0) mergedArray.push(anotherArray.shift());
-        if(anotherArray.length === 0) mergedArray.push(oneArray.shift());
-        if(oneArray[0] < anotherArray[0])
+        //Boolean(undefined comparisons) eval to false
+        if(oneArray[0] <= anotherArray[0])
             mergedArray.push(oneArray.shift());
-        else{
+        else if(oneArray[0] > anotherArray[0]){
             mergedArray.push(anotherArray.shift());
         }
+        if(oneArray.length === 0) {
+            mergedArray.push(...anotherArray);
+            anotherArray = [];
+        }
+        if(anotherArray.length === 0) {
+            mergedArray.push(...oneArray);
+            oneArray = [];
+        }
     }
+    console.log(`Merged into: ${mergedArray}`);
     return mergedArray;
 }
 
@@ -22,16 +30,15 @@ let mergeSort = arr => {
 
     let left = [];
     let right = [];
-    arr.forEach((element, index) => {
-        if (index < (arr.length)/2)
-            left.push(element);
-        else
-            right.push(element);
-    });
+    let middle = arr.length/2;
+
+    arr.forEach((element, index) => index < middle ? left.push(element) : right.push(element));
     console.log(`Left: ${left}, Right: ${right}`)
+
     left = mergeSort(left);
     right = mergeSort(right);
-    console.log(`MERGING Left: ${left}, Right: ${right}`)
+    console.log(`Merging -> Left: ${left} ---- Right: ${right}`)
+    
     return merge(left, right);
 }
 
