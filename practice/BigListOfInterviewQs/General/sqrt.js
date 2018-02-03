a2e4618ca1336f78bba8cc43166766de4ed0ee0a
Michael Neas <michael.r.neas@gmail.com>
2018-02-02 19:33:04 -0500

    added the difference between approx and node implementation

--- practice/BigListOfInterviewQs/General/sqrt.js
+++ practice/BigListOfInterviewQs/General/sqrt.js
@@ -12,13 +12,14 @@ let squareRoot = n => {
 }
 
 
-console.log(squareRoot(50))
-console.log(squareRoot(49))
-console.log(squareRoot(123))
-console.log(squareRoot(5123))
-console.log(squareRoot(5))
-console.log(squareRoot(9))
-console.log(squareRoot(120))
-console.log(squareRoot(490))
-console.log(squareRoot(54))
-console.log(squareRoot(90))
+console.log(`Difference for approximation of sqrt 50 ${squareRoot(50)- Math.sqrt(50)}`);
+console.log(`Difference for approximation of sqrt 49 ${squareRoot(49)- Math.sqrt(49)}`);
+console.log(`Difference for approximation of sqrt 123 ${squareRoot(123)- Math.sqrt(123)}`);
+console.log(`Difference for approximation of sqrt 5123 ${squareRoot(5123)- Math.sqrt(5123)}`);
+console.log(`Difference for approximation of sqrt 5 ${squareRoot(5)- Math.sqrt(5)}`);
+console.log(`Difference for approximation of sqrt 9 ${squareRoot(9) - Math.sqrt(9)}`);
+console.log(`Difference for approximation of sqrt 120 ${squareRoot(120)- Math.sqrt(120)}`);
+console.log(`Difference for approximation of sqrt 490 ${squareRoot(490)- Math.sqrt(490)}`);
+console.log(`Difference for approximation of sqrt 54 ${squareRoot(54)- Math.sqrt(54)}`);
+console.log(`Difference for approximation of sqrt 90 ${squareRoot(90)- Math.sqrt(90)}`);
+console.log(`Difference for approximation of sqrt 4 ${squareRoot(4) - Math.sqrt(4)}`);
