c091f788318dd9547a34f1be785b326e90644ace
Michael Neas <MichaelNeas@users.noreply.github.com>
2018-03-27 23:10:04 -0400

    Update protocol.swift

--- swift/random/protocol.swift
+++ swift/random/protocol.swift
@@ -2,6 +2,7 @@
 // and other requirements that suit a particular task or piece of functionality. 
 // The protocol can then be adopted by a class, structure, or enumeration to provide an actual implementation of those requirements. 
 // Any type that satisfies the requirements of a protocol is said to conform to that protocol.
+//https://medium.com/@abhimuralidharan/all-about-protocols-in-swift-11a72d6ea354
 
 class aClass: ParentClass, Protocol1, Protocol2 {}
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {}
