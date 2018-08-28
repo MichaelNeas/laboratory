7c8b63ae2be87850c38064a91996778e26900061
michaelneas <michael.r.neas@gmail.com>
2018-08-28 10:50:38 -0400

    Specialized equivalence of strings, anagram with a twist

---
+++ practice/L33t/893specialEquivalentStrings.js
@@ -0,0 +1,28 @@
+/** not the fastest but kind of cool
+ * Odds can swap with odds, evens can swap with evens, twist on anagram
+ * You are given an array A of strings.
+ * Two strings S and T are special-equivalent if after any number of moves, S == T.
+ * A move consists of choosing two indices i and j with i % 2 == j % 2, and swapping S[i] with S[j].
+ * Now, a group of special-equivalent strings from A is a non-empty subset S of A such that any string not in S is not special-equivalent with any string in S.
+ * Return the number of groups of special-equivalent strings from A.
+ * @param {string[]} A
+ * @return {number}
+ */
+var numSpecialEquivGroups = function(A) {
+    return A.reduce((prev, curr) => {
+        let left = ""
+        let right = ""
+        for(let i=0; i<curr.length; i++){
+            i%2===0 ? left += curr[i] : right += curr[i] 
+        }
+        left = [...left].sort().join("")
+        right = [...right].sort().join("")
+        for(let i=0; i<prev.length; i++){
+            if(prev[i][0] === left && prev[i][1] === right){
+                return prev
+            }
+        }
+        prev.push([left,right])
+        return prev
+    }, []).length
+};
