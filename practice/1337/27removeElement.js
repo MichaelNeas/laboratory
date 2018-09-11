4b2069132d132aa2f179d313b7c88cd5dea93bad
michaelneas <michael.r.neas@gmail.com>
2018-09-10 22:09:52 -0400

    remove all occurances from an array by modifying the reference

---
+++ practice/1337/27removeElement.js
@@ -0,0 +1,17 @@
+/**
+ * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] === val) {
+            nums.splice(i,1)
+            i--
+        }
+    }
+    return nums.length
+};
