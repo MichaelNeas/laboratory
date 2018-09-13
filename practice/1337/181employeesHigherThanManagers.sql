e48e307bf8967b484ac3ec4151f2e9b5d109a102
michaelneas <michael.r.neas@gmail.com>
2018-09-12 22:07:22 -0400

    Sql query for getting employees who make more than managers

---
+++ practice/1337/181employeesHigherThanManagers.sql
@@ -0,0 +1,2 @@
+# The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+SELECT E1.Name as Employee FROM Employee as E1, Employee as E2 WHERE E1.ManagerId = E2.Id AND E1.Salary > E2.Salary
