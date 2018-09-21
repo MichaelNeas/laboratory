04c12d9e01d5ae5bd2c09f06092e9820efc93041
michaelneas <michael.r.neas@gmail.com>
2018-09-20 22:10:31 -0400

    Find all the customers who have never ordered

---
+++ practice/1337/183customersNeverOrdered.sql
@@ -0,0 +1,2 @@
+# Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+SELECT Name AS Customers FROM Customers WHERE Id NOT IN (SELECT CustomerId FROM Orders)
