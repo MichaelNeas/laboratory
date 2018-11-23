5d3673960e6d12adcefb13bc38b4549af92d3bfc
michaelneas <michael.r.neas@gmail.com>
2018-11-22 21:07:20 -0500

    kicks over the 99 swift problems to the forked repo

--- practice/swift/swift-problems/Solutions/michaelneas/includes/main.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-import SolutionTester
-let tester = SolutionTester()
-tester.testP31()
-tester.showResults()
-exit(tester.exitCode)
--- practice/swift/swift-problems/Sources/runner/main.swift
+++
@@ -1,8 +0,0 @@
-import Command
-
-var registry = CommandRegistry(usage: "<command> <options>", overview: "99 Swift Problems command line tool")
-
-registry.register(command: StartCommand.self)
-registry.register(command: UsernameCommand.self)
-
-registry.run()
