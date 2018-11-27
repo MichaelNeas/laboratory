73a7a74139a8334589de9bccb6169c3e3430ad11
michaelneas <michael.r.neas@gmail.com>
2018-11-26 22:08:28 -0500

    refresher on map reduce filter in node

--- practice/jsthings-mpj/random.js
+++ practice/jsthings-mpj/random.js
@@ -1,7 +1,7 @@
 //nothing to see heres
 let fs = require('fs');
 
-let out = fs.readFileSync('random.txt', 'utf8')
+let out = fs.readFileSync(`${__dirname}/random.txt`, 'utf8')
     .trim()
     .split('\n')
     .map(line => line.split("    "))
@@ -18,4 +18,15 @@ let out = fs.readFileSync('random.txt', 'utf8')
 console.log(out)
 
 //Random way to check for bad words!
+let str = "fudge"
+let censoredList = ['cycles']
 let res = str.replace(/\W/g, ' ').split(' ').filter(val => !!~censoredList.indexOf(val));
+
+console.log(res)
+
+let bleh = [1,5,3,67,3,4,5,61,4,2,3]
+            .map(val => val * 3)
+            .filter(val => val % 2 === 0 )
+            .reduce((prev, curr) => prev + curr, 0)
+
+console.log(bleh)
