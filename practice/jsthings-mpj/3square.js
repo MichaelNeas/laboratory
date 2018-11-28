03fa83d65f7940fb0bc6e6eaa0da5ca0e70b727b
michaelneas <michael.r.neas@gmail.com>
2018-11-27 22:33:26 -0500

    wow functions

--- practice/jsthings-mpj/3square.js
+++ practice/jsthings-mpj/3square.js
@@ -3,3 +3,7 @@ var numbers = [1,2,3,4,5,6,7,8,9];
 for(var i=0;i<numbers.length;i++){
 	console.log(numbers[i]);
 }
+console.log('hiii')
+numbers.forEach(console.log)
+console.log('wow that separates a lot')
+console.log(numbers.find(val => val % 2))
