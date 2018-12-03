37e95eb4b5a221a9de8f558977cbafca1992598a
michaelneas <michael.r.neas@gmail.com>
2018-12-02 20:20:38 -0500

    day 2 code advent complete

--- practice/advent-of-code/2018/day2.swift
+++ practice/advent-of-code/2018/day2.swift
@@ -1,5 +1,35 @@
 import Foundation
 
+extension String {
+    func charAt(_ at: Int) -> Character {
+        let charIndex = self.index(self.startIndex, offsetBy: at)
+        return self[charIndex]
+    }
+}
+
+func pullOutDifference(between a: String, and b: String) -> String{
+    var solution = ""
+    for var letter in 0..<a.count{
+        if a.charAt(letter) == b.charAt(letter) {
+            solution += String(a.charAt(letter))
+        }
+    }
+    return solution
+}
+
+func compareLetterDiff(between a: String, and b: String) -> Bool{
+    var offByOne = false
+    for var letter in 0..<a.count{
+        if a.charAt(letter) != b.charAt(letter) {
+            if offByOne {
+                return false
+            }
+            offByOne = true
+        }
+    }
+    return offByOne
+}
+
 let location = "/Users/michaelneas/workspace/laboratory/practice/advent-of-code/2018/day2data.txt"
 do {
     let fileContent = try String(contentsOfFile: location, encoding: .utf8)
@@ -25,7 +55,19 @@ do {
     })
 
     print(stringChecksum.0 * stringChecksum.1)
+
+    // part 2
+    let stringChecksums = fileContent.components(separatedBy: .newlines)
+    .compactMap { String($0) }
+    for var x in 0..<stringChecksums.count {
+        for var y in x..<stringChecksums.count {
+            if compareLetterDiff(between: stringChecksums[x], and: stringChecksums[y]) {
+                print(pullOutDifference(between: stringChecksums[x], and: stringChecksums[y]))
+            }
+        }
+    }
 }
 catch {
     print("Error reading text. \(error)")
 }
+
