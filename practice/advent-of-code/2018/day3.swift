0f669cde69087da471c3631cda83ab6989ab2c35
michaelneas <michael.r.neas@gmail.com>
2018-12-04 00:38:43 -0500

    day 3 not compiling yet

---
+++ practice/advent-of-code/2018/day3.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+let location = "/Users/michael.neas/workspace/laboratory/practice/advent-of-code/2018/data/day3.txt"
+do {
+    let fileContent = try String(contentsOfFile: location, encoding: .utf8)
+    let separatorSet = CharacterSet(charactersIn: " #@,:x")
+    let allCuts = fileContent.components(separatedBy: .newlines)
+    .compactMap { String($0).components(separatedBy: separatorSet)
+    .compactMap { Int($0) } }
+    
+    var cutMap:[String:Int] = [String : Int]()
+    for cut in allCuts {
+        for i in cut[1]..<cut[1]+cut[3]{
+            for j in cut[2]..<cut[2]+cut[4]{
+                 cutMap["\(i),\(j)"] = cutMap["\(i),\(j)"] == nil ? 1 : cutMap["\(i),\(j)"]! + 1
+            }
+        }
+    }
+    print(cutMap)    
+}
+catch {
+    print("Error reading text. \(error)")
+}
