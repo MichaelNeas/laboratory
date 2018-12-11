14e72f98ce2958b2217bce7de91b55762b07a77e
michaelneas <michael.r.neas@gmail.com>
2018-12-10 21:50:19 -0500

    topo variant it is

--- practice/advent-of-code/2018/day7.swift
+++ practice/advent-of-code/2018/day7.swift
@@ -2,33 +2,18 @@ import Foundation
 let location = "/Users/michaelneas/workspace/laboratory/practice/advent-of-code/2018/data/day7.txt"
 struct Node {
     var value: String
-    var children: [Node]
 }
 
-struct Tree {
-    var nodes: [Node]
+struct Graph {
+    var nodes: [String: [String]]
+    var verticies: Int
 
-    mutating func insertNode(_ n: Node){
-        var node = n
-        var queue = nodes
-        while queue.count > 0 {
-            var curr = queue.removeFirst()
-            if curr.value == node.value {
-                curr.children.append(node)
-                return
-            } else if curr.value == node.children[0].value {
-                _ = node.children.removeFirst()
-                node.children.append(curr)
-                curr = node
-                return
-            }
-            if curr.children.count > 0 {
-                for child in curr.children {
-                    queue.append(child)
-                }
-            }
+    mutating func insertNode(from: Node, to: Node){
+        if self.nodes[from.value] != nil {
+            self.nodes[from.value]?.append(to.value)
+        } else {
+            self.nodes[from.value] = [to.value]
         }
-        nodes.append(node)
     }
 
     func details(){
@@ -41,16 +26,16 @@ do {
     let fileContent = try String(contentsOfFile: location, encoding: .utf8)
     var ordering = fileContent.components(separatedBy: .newlines)
     .compactMap { String($0).components(separatedBy: " ") }
-    .map { Node(value: $0[1], children: [Node(value: $0[7], children: [])]) }
+    .map { (Node(value: $0[1]), Node(value: $0[7])) }
     
-    var tree = Tree(nodes: [ordering.removeFirst()])
+    var graph = Graph(nodes: [String:[String]]() , verticies: 0)
 
-    tree.insertNode(ordering.removeFirst())
-    // for node in ordering {
-    //     tree.insertNode(node)
-    // }
+    
+    for node in ordering {
+        graph.insertNode(from: node.0,to: node.1)
+    }
 
-    tree.details()
+    graph.details()
 }
 catch {
     print("Error reading text. \(error)")
