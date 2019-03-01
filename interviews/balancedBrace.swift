678a6acf27a766c4c45aa5f21bbfbe9f8369b7ca
michaelneas <michael.r.neas@gmail.com>
2019-03-01 13:39:01 -0500

    ak sol

--- interviews/balancedBrace.swift
+++ interviews/balancedBrace.swift
@@ -15,3 +15,17 @@ func isBalanced(input: String) -> Bool {
     }
     return stack.count == 0
 }
+
+func akIsBalanced(str: String) -> Bool {
+    var open = 0
+    var closed = 0
+    for char in str {
+        if char == "(" {
+            open += 1
+        } else if sub == ")" {
+            closed += 1
+            guard closed <= open else { return false }
+        }
+    }
+    return open == closed
+}
