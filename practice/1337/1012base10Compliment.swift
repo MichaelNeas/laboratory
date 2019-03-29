bb241552b053288b40d588ea9e5c7d0b155a4f16
michaelneas <michael.r.neas@gmail.com>
2019-03-29 11:47:53 -0400

    1012 base 10 compliment solution

---
+++ practice/1337/1012base10Compliment.swift
@@ -0,0 +1,22 @@
+/// Every non-negative integer N has a binary representation.  
+/// For example, 5 can be represented as "101" in binary, 11 as "1011" in binary, and so on.  
+/// Note that except for N = 0, there are no leading zeroes in any binary representation.
+/// The complement of a binary representation is the number in binary you get when changing every 1 to a 0 and 0 to a 1.  
+/// For example, the complement of "101" in binary is "010" in binary.
+/// For a given number N in base-10, return the complement of it's binary representation as a base-10 integer.
+
+class Solution {
+    func bitwiseComplement(_ N: Int) -> Int {
+        let str = String(N, radix: 2)
+        var result = ""
+        for bin in str {
+            result += bin == "1" ? "0" : "1"
+            
+        }
+        
+        if let number = Int(result, radix: 2) { 
+            return number
+        }
+        return 0
+    }
+}
