0ac7874ae1052a443281c771427b53ece6227098
michaelneas <michael.r.neas@gmail.com>
2019-04-02 13:18:34 -0400

    bounds notes

--- practice/1337/771jewelsAndStones.swift
+++ practice/1337/771jewelsAndStones.swift
@@ -2,6 +2,10 @@
 /// Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
 /// The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, 
 /// so "a" is considered a different type of stone from "A".
+/// Formula is [mapping ops] + [lookups]
+/// Best case: 1 + 1 = 2
+/// Worst case: 50 + 50 = 100
+/// So we can tight bound at T(1)
 
 class Solution {
     func numJewelsInStones(_ J: String, _ S: String) -> Int {
