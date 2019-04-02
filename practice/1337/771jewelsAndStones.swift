5db9c39975c8fd34298349a50baf2ccd8d29c305
michaelneas <michael.r.neas@gmail.com>
2019-04-02 13:18:07 -0400

    jewels and stones but swift

---
+++ practice/1337/771jewelsAndStones.swift
@@ -0,0 +1,11 @@
+/// You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
+/// Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+/// The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, 
+/// so "a" is considered a different type of stone from "A".
+
+class Solution {
+    func numJewelsInStones(_ J: String, _ S: String) -> Int {
+        let jewels = Set(Array(J))
+        return Array(S).reduce(0, { jewels.contains($1) ? $0+1 : $0 })
+    }
+}
