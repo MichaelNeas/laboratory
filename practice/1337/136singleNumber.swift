31122dfc0facd7c38658f4c911ea54c47ac7a8bd
michaelneas <michael.r.neas@gmail.com>
2019-04-24 14:56:22 -0400

    136 but swift two ways

---
+++ practice/1337/136singleNumber.swift
@@ -0,0 +1,17 @@
+/// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+class Solution {
+    func singleNumber(_ nums: [Int]) -> Int {
+        let sortedNums = nums.sorted()
+        let sequence = stride(from: 0, to: sortedNums.count, by: 2)
+        for i in sequence {
+            if i == sortedNums.count - 1 || sortedNums[i] != sortedNums[i+1] {
+                return sortedNums[i]
+            }
+        }
+        return -1
+    }
+
+    func singleNumberReduce(_ nums: [Int]) -> Int {
+        return nums.reduce(0, { $0 ^ $1 })
+    }
+}
