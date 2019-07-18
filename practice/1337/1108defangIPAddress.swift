9cc96a02e22900fac677fa9f3b9082ebc0b49bf3
michaelneas <michael.r.neas@gmail.com>
2019-07-18 12:59:50 -0400

    alternate solutions to 1108

--- practice/1337/1108defangIPAddress.swift
+++ practice/1337/1108defangIPAddress.swift
@@ -2,11 +2,18 @@
 /// A defanged IP address replaces every period "." with "[.]".
 
 class Solution {
+    // slowest
     func defangIPaddr(_ address: String) -> String {
-        let regex = try! NSRegularExpression(pattern: "\\.", options: NSRegularExpression.Options.caseInsensitive)
+        let regex = try! NSRegularExpression(pattern: "\\.")
         return regex.stringByReplacingMatches(in: address, options: [], range: NSMakeRange(0, address.count), withTemplate: "[.]")
     }
 
+    // middle
+    func defangIPaddr(_ address: String) -> String {
+        return address.reduce(String() , { $0 + ($1 == "." ? "[.]" : String($1)) })
+    }
+
+    // fastest
     func defangIPaddr(_ address: String) -> String {
         return address.replacingOccurrences(of: ".", with: "[.]")
     }
