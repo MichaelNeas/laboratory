b984cc8167522a8f0aa5573ddc2aac6c2b58dfb0
michaelneas <michael.r.neas@gmail.com>
2019-10-01 00:43:23 -0400

    websocket class and view controller

--- practice/swift/projects/AllTheNetworks/AllTheNetworks/ViewController.swift
+++ practice/swift/projects/AllTheNetworks/AllTheNetworks/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var messageBox: UITextView!
+    
+    var webSocketTask: WebSocket?
+    var wsurlString = "ws://localhost:3000/socket.io/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        webSocketTask = WebSocket(url: URL(string: wsurlString)!)
+        webSocketTask?.connect()
     }
-
-
+    
+    @IBAction func sendButton(_ sender: Any) {
+        webSocketTask?.send(text: textField.text!)
+    }
+    
+    @IBAction func disconnect(_ sender: Any) {
+        webSocketTask?.disconnect()
+    }
+    
 }
-
--- practice/swift/projects/AllTheNetworks/AllTheNetworks/WebSocket.swift
+++ practice/swift/projects/AllTheNetworks/AllTheNetworks/WebSocket.swift
@@ -7,3 +7,131 @@
 //
 
 import Foundation
+import Combine
+import Network
+
+protocol WebSocketConnection {
+    func send(text: String)
+    func send(data: Data)
+    func connect()
+    func disconnect()
+    var delegate: WebSocketConnectionDelegate? {
+        get
+        set
+    }
+}
+
+protocol WebSocketConnectionDelegate {
+    func onConnected(connection: WebSocketConnection)
+    func onDisconnected(connection: WebSocketConnection, error: Error?)
+    func onError(connection: WebSocketConnection, error: Error)
+    func onMessage(connection: WebSocketConnection, text: String)
+    func onMessage(connection: WebSocketConnection, data: Data)
+}
+
+class WebSocket: NSObject, WebSocketConnection, URLSessionWebSocketDelegate {
+    var delegate: WebSocketConnectionDelegate?
+    var webSocketTask: URLSessionWebSocketTask!
+    var urlSession: URLSession!
+    let delegateQueue = OperationQueue()
+    var sid = ""
+    var components: URLComponents!
+    
+    init(url: URL) {
+        super.init()
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        components!.queryItems = [
+            URLQueryItem(name: "EIO", value: "3"),
+            URLQueryItem(name: "transport", value: "websocket"),
+            URLQueryItem(name: "sid", value: sid)
+        ]
+        urlSession = URLSession(configuration: .default, delegate: self, delegateQueue: delegateQueue)
+        webSocketTask = urlSession.webSocketTask(with: components!.url!)
+    }
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        self.delegate?.onConnected(connection: self)
+    }
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        self.delegate?.onDisconnected(connection: self, error: nil)
+    }
+    
+    func connect() {
+        // required to open socket
+        webSocketTask.resume()
+        listen()
+    }
+    
+    func disconnect() {
+        webSocketTask.cancel(with: .goingAway, reason: nil)
+    }
+    
+    func listen()  {
+        webSocketTask.receive { result in
+            switch result {
+            case .failure(let error):
+                self.delegate?.onError(connection: self, error: error)
+            case .success(let message):
+                switch message {
+                case .string(let text):
+                    if text.first == "0" {
+                        self.handleHandShake(response: text)
+                    }
+                    self.delegate?.onMessage(connection: self, text: text)
+                case .data(let data):
+                    self.delegate?.onMessage(connection: self, data: data)
+                @unknown default:
+                    fatalError()
+                }
+                
+                self.listen()
+            }
+        }
+    }
+    
+    func handleHandShake(response: String) {
+        var response = response
+        response.removeFirst()
+        let websocketHandShake = try? JSONDecoder().decode(WebsocketHandShake.self, from: response.data(using: .utf8)!)
+        sid = String(websocketHandShake!.sid)
+        print("Received string: \(sid)")
+        connect()
+        sendPing()
+    }
+    
+    func sendPing() {
+        webSocketTask.sendPing { (error) in
+          if let error = error {
+            print("Sending PING failed: \(error)")
+          }
+
+          DispatchQueue.main.asyncAfter(deadline: .now() + 1.0 ) {
+            self.sendPing()
+          }
+        }
+    }
+    
+    func send(text: String) {
+        webSocketTask.send(URLSessionWebSocketTask.Message.string(text)) { error in
+            if let error = error {
+                self.delegate?.onError(connection: self, error: error)
+            }
+        }
+    }
+    
+    func send(data: Data) {
+        webSocketTask.send(URLSessionWebSocketTask.Message.data(data)) { error in
+            if let error = error {
+                self.delegate?.onError(connection: self, error: error)
+            }
+        }
+    }
+}
+
+// MARK: - WebsocketHandShake
+struct WebsocketHandShake: Codable {
+    let sid: String
+    let pingInterval: Int
+    let pingTimeout: Int
+}
