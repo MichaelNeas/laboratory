cc000bfc1e8a6a46aea820d8e9877bba3fe522dc
michaelneas <michael.r.neas@gmail.com>
2019-09-30 21:21:13 -0400

    websocket node server, new xcodeproj for websocket iphone

---
+++ practice/swift/projects/AllTheNetworks/AllTheNetworks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllTheNetworks
+//
+//  Created by Michael Neas on 9/30/19.
+//  Copyright © 2019 Neas Lease. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ practice/swift/projects/AllTheNetworks/nodesocket/index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
