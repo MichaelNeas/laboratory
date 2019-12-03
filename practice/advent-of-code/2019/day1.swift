badfad3fbfb53282e394963e459ba544851cdf31
michaelneas <michael.r.neas@gmail.com>
2019-12-02 21:51:53 -0500

    memoize day 1

--- practice/advent-of-code/2019/day1.swift
+++ practice/advent-of-code/2019/day1.swift
@@ -7,17 +7,25 @@ do {
     let fuelFunc = { val in
         return ((val / 3) - 2)
     }
+    var cache = [Int:Int]()
+
+    func findFuelSum(fuel: Int) -> Int {
+        if let val = cache[fuel] {
+            return val
+        }
+        let fuelProcessed = fuelFunc(fuel)
+        if fuelProcessed > 0 {
+            let fuelSum = fuelProcessed + findFuelSum(fuel: fuelProcessed)
+            cache[fuel] = fuelSum
+            return fuelSum
+        }
+        return 0
+    }
+
     let totalFuel = fileContent.components(separatedBy: .newlines)
         .compactMap { Int($0) }
-        .reduce(0, { sum, curr in
-            var sum = sum
-            var temp = fuelFunc(curr)
-            while temp > 0 {
-                sum += temp
-                temp = fuelFunc(temp)
-            }
-            return sum
-        })
+        .reduce(0, { $0 + findFuelSum(fuel: $1)})
+    
     print(totalFuel)
     
 }
