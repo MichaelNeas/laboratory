6df53536e4f5911fd404a8578e6126783e5d7647
michaelneas <michael.r.neas@gmail.com>
2019-12-05 12:36:01 -0500

    day 5 part 2

--- practice/advent-of-code/2019/day5.swift
+++ practice/advent-of-code/2019/day5.swift
@@ -14,9 +14,13 @@ do {
     
     func mode(value: Int) -> InstructionMode {
         let modeConfig = String(format: "%05d", value)
+        // third param
         let A = Int(String(modeConfig[modeConfig.index(modeConfig.startIndex, offsetBy: 0)]))! == 1
+        // second param
         let B = Int(String(modeConfig[modeConfig.index(modeConfig.startIndex, offsetBy: 1)]))! == 1
+        // first param
         let C = Int(String(modeConfig[modeConfig.index(modeConfig.startIndex, offsetBy: 2)]))! == 1
+        // opcode
         let DE = Int(modeConfig[modeConfig.index(modeConfig.startIndex, offsetBy: 3)...])!
         return (A, B, C, DE)
     }
@@ -42,16 +46,45 @@ do {
             i += 4
         // store
         case 3:
-            print("stored 1")
-            list[list[i+1]] = 1
+            print("stored 5")
+            list[list[i+1]] = 5
             i += 2
         // output
         case 4:
             print(list[list[i+1]])
             i += 2
+        // jump if true
+        case 5:
+            if (currentMode.C ? list[i+1] : list[list[i+1]]) != 0 {
+                i = (currentMode.B ? list[i+2] : list[list[i+2]])
+            } else {
+                i += 3
+            }
+        // jump if false
+        case 6:
+            if (currentMode.C ? list[i+1] : list[list[i+1]]) == 0 {
+                i = (currentMode.B ? list[i+2] : list[list[i+2]])
+            } else {
+                i += 3
+            }
+        // less than
+        case 7:
+            if currentMode.A {
+                list[i + 3] = ((currentMode.C ? list[i+1] : list[list[i+1]]) < (currentMode.B ? list[i+2] : list[list[i+2]])) ? 1 : 0
+            } else {
+                list[list[i+3]] = ((currentMode.C ? list[i+1] : list[list[i+1]]) < (currentMode.B ? list[i+2] : list[list[i+2]])) ? 1 : 0
+            }
+            i += 4
+        // equals
+        case 8:
+            if currentMode.A {
+                list[i + 3] = ((currentMode.C ? list[i+1] : list[list[i+1]]) == (currentMode.B ? list[i+2] : list[list[i+2]])) ? 1 : 0
+            } else {
+                list[list[i+3]] = ((currentMode.C ? list[i+1] : list[list[i+1]]) == (currentMode.B ? list[i+2] : list[list[i+2]])) ? 1 : 0
+            }
+            i += 4
         default:
             break
-            //print(currentMode)
         }
         currentMode = mode(value: list[i])
     }
