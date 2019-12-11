d5107fe07361fe25284687384bc60fcda737db23
michaelneas <michael.r.neas@gmail.com>
2019-12-10 21:53:43 -0500

    enough time on part 2, maybe pick it up again another day

--- challenges/advent-of-code/2019/day10.swift
+++ challenges/advent-of-code/2019/day10.swift
@@ -17,34 +17,43 @@ do {
         }
     }
 
-    var foundAstroids = [Astroid]()
+    //var foundAstroids = [Astroid]()
     func arcTan(_ x: Int, _ y: Int, _ u: Int, _ v: Int) -> Double {
         return atan2(Double(v-y), Double(u-x)) * 180.0 / Double.pi
     }
-    
+    var foundCoordinates = [(Int,Int)]()
     func checkFrom(_ x: Int, _ y: Int) -> Int {
-        var foundAngles = Set<Double>()
+        //var foundAngles = Set<Double>()
         for u in 0..<astroids[0].count {
             for v in 0..<astroids.count {
-                if astroids[v][u] == "#" {
-                    if x == 5 && y == 8 {
-                        print(arcTan(x, y, u, v))
-                    }   
-                    foundAngles.insert(arcTan(x, y, u, v))
+                if astroids[v][u] == "#" && (v != y || u != x) {
+                    foundCoordinates.append((u - x, v - y))
+                    //foundAngles.append(arcTan(x, y, u, v))
+                    //foundAngles.insert(arcTan(x, y, u, v))
                 }
             }
         }
-        return foundAngles.count
+        
+        return foundCoordinates.count
     }
 
-    for x in 0..<astroids[0].count {
-        for y in 0..<astroids.count {
-            if astroids[y][x] == "#" {
-                foundAstroids.append(Astroid(x: x, y: y, visibleAstroids: checkFrom(x,y)))
-            }
-        }
-    }
-    print(foundAstroids.sorted(by: { $0.visibleAstroids > $1.visibleAstroids }))
+    // for x in 0..<astroids[0].count {
+    //     for y in 0..<astroids.count {
+    //         if astroids[y][x] == "#" {
+    //             foundAstroids.append(Astroid(x: x, y: y, visibleAstroids: checkFrom(x,y)))
+    //         }
+    //     }
+    // }
+    let count = checkFrom(22,25)
+    let distanceAngles = foundCoordinates.map{ (sqrt(Double($0.0 * $0.0) + Double($0.1 * $0.1)), atan2(Double($0.1), Double($0.0))) }
+    print(distanceAngles)
+    print(count)
+    //print(foundAstroids.sorted(by: { $0.visibleAstroids > $1.visibleAstroids }))
+    // var destroyedAstroids = 0
+    // var angle = 0
+    // while foundAstroids < destroyedAstroids {
+
+    // }
 }
 catch {
     print("Error reading text. \(error)")
