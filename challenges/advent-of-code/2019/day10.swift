d5bb21f5a022d49593d0e169cc3eadc2b8854326
michaelneas <michael.r.neas@gmail.com>
2019-12-10 20:55:09 -0500

    day 10 part 1 arctan2 the rescue

--- challenges/advent-of-code/2019/day10.swift
+++ challenges/advent-of-code/2019/day10.swift
@@ -5,17 +5,46 @@ let dataURL = URL(fileURLWithPath: "2019/data/day10.txt", relativeTo: currentDir
 
 do {
     let fileContent = try String(contentsOf: dataURL, encoding: .utf8)
-    let astroidMap = fileContent.components(separatedBy: "\n").compactMap { Array($0) }
+    let astroids = fileContent.components(separatedBy: "\n").compactMap { Array($0) }
 
-    for x in 0..<astroidMap.count {
-        for y in 0..<astroidMap[0].count{
-            print("\(x),\(y) : \(astroidMap[x][y])")
+    struct Astroid: CustomDebugStringConvertible {
+        let x: Int
+        let y: Int
+        let visibleAstroids: Int
+
+        var debugDescription: String {
+            return "Astroid [\(x), \(y)] => \(visibleAstroids)"
         }
     }
-    print(astroidMap)
-    //[.] empty 
-    //[#] astroid, X,Y where X is distance from left and Y is from top where 0,0 is top left
-    // calculate the angle to all other astroids
+
+    var foundAstroids = [Astroid]()
+    func arcTan(_ x: Int, _ y: Int, _ u: Int, _ v: Int) -> Double {
+        return atan2(Double(v-y), Double(u-x)) * 180.0 / Double.pi
+    }
+    
+    func checkFrom(_ x: Int, _ y: Int) -> Int {
+        var foundAngles = Set<Double>()
+        for u in 0..<astroids[0].count {
+            for v in 0..<astroids.count {
+                if astroids[v][u] == "#" {
+                    if x == 5 && y == 8 {
+                        print(arcTan(x, y, u, v))
+                    }   
+                    foundAngles.insert(arcTan(x, y, u, v))
+                }
+            }
+        }
+        return foundAngles.count
+    }
+
+    for x in 0..<astroids[0].count {
+        for y in 0..<astroids.count {
+            if astroids[y][x] == "#" {
+                foundAstroids.append(Astroid(x: x, y: y, visibleAstroids: checkFrom(x,y)))
+            }
+        }
+    }
+    print(foundAstroids.sorted(by: { $0.visibleAstroids > $1.visibleAstroids }))
 }
 catch {
     print("Error reading text. \(error)")
