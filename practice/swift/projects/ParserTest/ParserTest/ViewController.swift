414607923970d443ad3772ca841eb9b7c1c67230
michaelneas <michael.r.neas@gmail.com>
2020-04-08 18:09:47 -0400

    Preserve attributes while adding font

--- practice/swift/projects/ParserTest/ParserTest/ViewController.swift
+++ practice/swift/projects/ParserTest/ParserTest/ViewController.swift
@@ -30,11 +30,38 @@ class ViewController: UIViewController {
         let htmlFile = Bundle.main.path(forResource: "nicetomeetya", ofType: "html")!
         let contents = try! String(contentsOfFile: htmlFile)
         let htmlData = NSString(string: contents).data(using: String.Encoding.unicode.rawValue)
-        let options = [NSAttributedString.DocumentReadingOptionKey.documentType:
-                NSAttributedString.DocumentType.html]
-        htmlView.attributedText = try? NSMutableAttributedString(data: htmlData ?? Data(),
+        let options = [
+            NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html,
+        ]
+        
+        let font = UIFont.systemFont(ofSize: 12)
+        let boldFont = UIFont.boldSystemFont(ofSize: 14)
+        let shadow = NSShadow()
+        shadow.shadowColor = UIColor.red
+        shadow.shadowBlurRadius = 5
+        
+         let attributedHtml = try! NSMutableAttributedString(data: htmlData ?? Data(),
                                                                   options: options,
                                                                   documentAttributes: nil)
+        
+        attributedHtml.beginEditing()
+        attributedHtml.enumerateAttribute(NSAttributedString.Key.font, in: NSRange(location:0, length: attributedHtml.length), options: NSAttributedString.EnumerationOptions(rawValue: 0), using: { (value, range, stop) in
+                guard let value = value as? UIFont else { return }
+                //let fontName = value.fontName.lowercased()
+                var destinationFont = font
+                let descriptor = value.fontDescriptor
+                let symTraits = descriptor.symbolicTraits
+                if symTraits.contains(UIFontDescriptor.SymbolicTraits.traitBold) {
+                    destinationFont = boldFont
+                }
+                attributedHtml.removeAttribute(NSAttributedString.Key.font, range: range)
+                attributedHtml.addAttribute(.font, value: destinationFont, range: range)
+        })
+        attributedHtml.endEditing()
+        
+        //attributedHtml.addAttributes(attributes, range: NSRange(location:0, length: attributedHtml.length))
+        
+        htmlView.attributedText = attributedHtml
     }
 }
 
