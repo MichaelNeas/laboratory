b3b02f47b3ffddab69cf9b4659d283a371914a96
michaelneas <michael.r.neas@gmail.com>
2020-07-08 08:32:50 -0400

    adds functionality to instruct users to create new account when linking to anonymous

--- practice/swift/projects/AnonAuth/AnonAuth/ContentView.swift
+++ practice/swift/projects/AnonAuth/AnonAuth/ContentView.swift
@@ -12,6 +12,8 @@ import FirebaseAuth
 struct ContentView: View {
     @State var email: String = ""
     @State var password: String = ""
+    @State var createNewAccount = false
+    
     var body: some View {
         VStack {
             Text("Testin out anonymous users")
@@ -30,36 +32,63 @@ struct ContentView: View {
                 TextField("Password", text: $password)
                 Button(action: {
                     let credential = EmailAuthProvider.credential(withEmail: self.email, password: self.password)
-                    Auth.auth().currentUser?.link(with: credential, completion: { (authResult, error) in
-                        if let error = error {
-                            let authError = error as NSError
-                            if authError.code == AuthErrorCode.emailAlreadyInUse.rawValue {
-                                print(authError)
-                                let prevUser = Auth.auth().currentUser
-                                print("previous user: \(prevUser?.uid)")
-                                Auth.auth().signIn(with: credential, completion: { (authResult, error) in
-                                    print("error signing in with creds: \(error)")
-                                    if error == nil {
-                                        print("replace all data from \(prevUser?.uid) with \(Auth.auth().currentUser?.uid)")
-                                        prevUser?.delete(completion: { err in
-                                            print("error deleting user: \(err)")
+                    Auth.auth().fetchSignInMethods(forEmail: self.email, completion: { query, error in
+                        if error != nil {
+                            print("We got a big problem")
+                        } else if query != nil || self.createNewAccount == true {
+                            Auth.auth().currentUser?.link(with: credential, completion: { (authResult, error) in
+                                if let error = error {
+                                    let authError = error as NSError
+                                    if authError.code == AuthErrorCode.emailAlreadyInUse.rawValue {
+                                        let prevUser = Auth.auth().currentUser
+                                        print("previous user: \(prevUser?.uid)")
+                                        // try to sign in with credentials
+                                        Auth.auth().signIn(with: credential, completion: { (authResult, error) in
+                                            print("error signing in with creds: \(error)")
+                                            if error == nil {
+                                                print("replace all data from \(prevUser?.uid) with \(Auth.auth().currentUser?.uid)")
+                                                prevUser?.delete(completion: { err in
+                                                    print("error deleting user: \(err)")
+                                                })
+                                                print("retrieve all data")
+                                            } else if let error = error {
+                                                let authError = error as NSError
+                                                if authError.code == AuthErrorCode.wrongPassword.rawValue {
+                                                    print("Wrong password")
+                                                }
+                                            }
                                         })
+                                    } else if authError.code == AuthErrorCode.weakPassword.rawValue {
+                                        print("WEAK PW: \(authError)")
+                                    } else {
+                                        print(authError)
                                     }
-                                })
-                            }
+                                } else {
+                                    print("successfully authed and linked: \(authResult)")
+                                }
+                            })
                         } else {
-                            print("successfully authed: \(authResult)")
+                            // at this point a user needs to be directed to create an account
+                            print("Create an account")
                         }
                     })
-
                 }) {
-                    Text("Sign In, Link")
+                    if self.createNewAccount {
+                        Text("Create new account and Link")
+                    } else {
+                        Text("Sign In and Link")
+                    }
+                }
+                
+                Button(action: { self.createNewAccount.toggle() }) {
+                    if self.createNewAccount {
+                        Text("Will attempt to create new account")
+                    } else {
+                        Text("Will attempt to sign in normally")
+                    }
                 }
             }
         }
-        
-        
-        
     }
 }
 
