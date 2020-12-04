f91057374ca67c2b3c9ee74db7e0f5a3f521ee9e
michaelneas <michael.r.neas@gmail.com>
2020-12-04 09:10:28 -0500

    part 2 day 3 advent of code

--- challenges/advent-of-code/2020/day3.swift
+++ challenges/advent-of-code/2020/day3.swift
@@ -1,5 +1,19 @@
 import Foundation
 
+func treeFinder(treelines: [[Character]], x: Int, y: Int) -> Int {
+	var positionX = 0
+	var count = 0
+	let length = treelines[0].count
+
+	for positionY in stride(from: 0, to: treelines.count, by: y) {
+		if treelines[positionY][positionX % length] == "#" {
+			count += 1
+		}
+		positionX += x
+	}
+    return count 
+}
+
 let currentDirectoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
 let dataURL = URL(fileURLWithPath: "data/day3.txt", relativeTo: currentDirectoryURL)
 
@@ -8,17 +22,12 @@ do {
     let treelines = fileContent.components(separatedBy: .newlines)
         .compactMap { Array(String($0)) }
     
-	var positionX = 0
-	var count = 0
-	let length = treelines[0].count
-
-	for positionY in stride(from: 0, to: treelines.count, by: 1) {
-		if treelines[positionY][positionX % length] == "#" {
-			count += 1
-		}
-		positionX += 3
-	}
-    print(count)    
+	print(treeFinder(treelines: treelines, x: 1, y: 1) *
+	treeFinder(treelines: treelines, x: 3, y: 1) *
+	treeFinder(treelines: treelines, x: 5, y: 1) *
+	treeFinder(treelines: treelines, x: 7, y: 1) *
+	treeFinder(treelines: treelines, x: 1, y: 2)
+	)
 }
 catch {
     print("Error reading text. \(error)")
