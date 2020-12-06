36f060bae030fbbdf9305d45a5d4b3f230b2da90
michaelneas <michael.r.neas@gmail.com>
2020-12-06 10:03:15 -0500

    advent of code day 5 part 1

---
+++ challenges/advent-of-code/2020/day5.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+let currentDirectoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+let dataURL = URL(fileURLWithPath: "data/day5.txt", relativeTo: currentDirectoryURL)
+
+do {
+    let fileContent = try String(contentsOf: dataURL, encoding: .utf8)
+    let locations = fileContent.components(separatedBy: .newlines)
+        .compactMap { String($0) }
+	var max = 0
+	for location in locations {
+		var rowA = 0
+		var rowB = 127
+		var colA = 0
+		var colB = 7
+		for val in location {
+			if val == "B" {
+				rowA = Int((rowB + rowA) / 2) + 1
+				//print("\(rowA)-\(rowB)")
+			} else if val == "F" {
+				rowB = Int((rowB + rowA) / 2)
+				//print("\(rowA)-\(rowB)")
+			} else if val == "R" {
+				colA = Int((colB + colA) / 2) + 1
+				//print("\(colA)-\(colB)")
+			} else {
+				colB = Int((colB + colA) / 2)
+				//print("\(colA)-\(colB)")
+			}
+		}
+		let num = (rowA * 8) + colA
+		if max < num {
+			max = num
+		}
+	}
+	print(max)
+} catch {
+	print("Error")
+}
