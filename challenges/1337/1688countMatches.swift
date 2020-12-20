f1cc892cbc2e47f569b786f27e9789cd8fef3464
michaelneas <michael.r.neas@gmail.com>
2020-12-19 20:20:08 -0500

    1688 count of matches in a tourny

---
+++ challenges/1337/1688countMatches.swift
@@ -0,0 +1,22 @@
+// You are given an integer n, the number of teams in a tournament that has strange rules:
+//     If the current number of teams is even, each team gets paired with another team. A total of n / 2 matches are played, and n / 2 teams advance to the next round.
+//     If the current number of teams is odd, one team randomly advances in the tournament, and the rest gets paired. A total of (n - 1) / 2 matches are played, and (n - 1) / 2 + 1 teams advance to the next round.
+// Return the number of matches played in the tournament until a winner is decided.
+class Solution {
+    func numberOfMatches(_ n: Int) -> Int {
+        var teams = n
+        var matches = 0
+        while teams > 1 {
+            if teams % 2 == 0 {
+                teams /= 2
+                matches += teams
+            } else {
+                teams = (teams - 1) / 2
+                matches += teams
+                teams += 1
+            }
+            
+        }
+        return matches
+    }
+}
