ce5ed5112c17217450bb10bf145ee8eee4cec67d
michaelneas <michael.r.neas@gmail.com>
2021-11-20 10:40:20 -0700

    2037 minimum number of moves to seat everyone

---
+++ challenges/1337/2037minNumMovesToSeat.swift
@@ -0,0 +1,16 @@
+/// There are n seats and n students in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. You are also given the array students of length n, where students[j] is the position of the jth student.
+/// You may perform the following move any number of times:
+/// Increase or decrease the position of the ith student by 1 (i.e., moving the ith student from position x to x + 1 or x - 1)
+/// Return the minimum number of moves required to move each student to a seat such that no two students are in the same seat.
+/// Note that there may be multiple seats or students in the same position at the beginning.
+
+class Solution {
+    func minMovesToSeat(_ seats: [Int], _ students: [Int]) -> Int {
+        let sortedSeats = seats.sorted()
+        let sortedStudents = students.sorted()
+        
+        return (0..<seats.count).reduce(0, { acc, i in
+            acc + abs(sortedSeats[i] - sortedStudents[i])
+        })
+    }
+}
