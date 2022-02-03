d5b864a73278c5291f57bf7a843f430d2686c874
michaelneas <michael.r.neas@gmail.com>
2022-02-02 20:19:18 -0700

    2154 keep multiplying found values by two

---
+++ challenges/1337/2154multFound.swift
@@ -0,0 +1,28 @@
+// You are given an array of integers nums. You are also given an integer original which is the first number that needs to be searched for in nums.
+// You then do the following steps:
+// If original is found in nums, multiply it by two (i.e., set original = 2 * original).
+// Otherwise, stop the process.
+// Repeat this process with the new number as long as you keep finding the number.
+// Return the final value of original.
+
+class Solution {
+    func findFinalValue(_ nums: [Int], _ original: Int) -> Int {
+//         let db = nums.reduce(into: [Int: Bool](), { acc, curr in
+//             acc[curr] = true                                        
+//         })
+        
+//         var iteration = original
+        
+//         while db[iteration] != nil {
+//             iteration *= 2
+//         }
+        
+//         return iteration
+        
+        var iteration = original
+        while nums.contains(iteration) {
+            iteration *= 2
+        }
+        return iteration
+    }
+}
