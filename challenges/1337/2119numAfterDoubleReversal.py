127c9a40f870b4e8438482854fdd2ee6c0a147f9
michaelneas <michael.r.neas@gmail.com>
2022-02-03 19:52:51 -0700

    2119 a number after a double reversal python

---
+++ challenges/1337/2119numAfterDoubleReversal.py
@@ -0,0 +1,8 @@
+# Reversing an integer means to reverse all its digits.
+
+# For example, reversing 2021 gives 1202. Reversing 12300 gives 321 as the leading zeros are not retained.
+# Given an integer num, reverse num to get reversed1, then reverse reversed1 to get reversed2. Return true if reversed2 equals num. Otherwise return false.
+
+class Solution:
+    def isSameAfterReversals(self, num: int) -> bool:
+        return int(str(int(str(num)[::-1]))[::-1]) == num
