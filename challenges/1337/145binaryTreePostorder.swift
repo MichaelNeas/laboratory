ad823eabae72ad5b57d92f9afae77d8e9d8a9fde
michaelneas <michael.r.neas@gmail.com>
2022-07-17 20:57:19 -0600

    145 binary tree post order traversal

---
+++ challenges/1337/145binaryTreePostorder.swift
@@ -0,0 +1,23 @@
+// Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func postorderTraversal(_ root: TreeNode?) -> [Int] {
+        guard let root = root else { return [] }
+        return postorderTraversal(root.left) + postorderTraversal(root.right) + [root.val]
+    }
+}
