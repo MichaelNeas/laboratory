cc742addadbc84f7502d64020b4c8b539979a822
michaelneas <michael.r.neas@gmail.com>
2022-07-18 21:19:39 -0600

    metal and swiftui

---
+++ gfx/GameEngine/Shared/GameEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameEngineApp.swift
+//  Shared
+//
+//  Created by Michael Neas on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct GameEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MetalView()
+        }
+    }
+}
---
+++ gfx/GameEngine/Shared/GameView.swift
@@ -0,0 +1,7 @@
+import MetalKit
+
+class GameView: MTKView {
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
