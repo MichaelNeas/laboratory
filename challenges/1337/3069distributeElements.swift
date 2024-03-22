8ab2d04cbbe2be4052589ce67c74a49cc28bfe4a
michaelneas <michael.r.neas@gmail.com>
2024-03-21 19:50:08 -0600

    hehe

--- challenges/1337/3069distributeElements.swift
+++ challenges/1337/3069distributeElements.swift
@@ -1,7 +1,6 @@
-You are given a 1-indexed array of distinct integers nums of length n.
-
-You need to distribute all the elements of nums between two arrays arr1 and arr2 using n operations. In the first operation, append nums[1] to arr1. In the second operation, append nums[2] to arr2. Afterwards, in the ith operation:
-
+/// You are given a 1-indexed array of distinct integers nums of length n.
+/// You need to distribute all the elements of nums between two arrays arr1 and arr2 using n operations. 
+/// In the first operation, append nums[1] to arr1. In the second operation, append nums[2] to arr2. Afterwards, in the ith operation:
 /// If the last element of arr1 is greater than the last element of arr2, append nums[i] to arr1. Otherwise, append nums[i] to arr2.
 /// The array result is formed by concatenating the arrays arr1 and arr2. For example, if arr1 == [1,2,3] and arr2 == [4,5,6], then result = [1,2,3,4,5,6].
 /// Return the array result.
