d8a2cabf6044a237258b6dd59c81704d553c5bee
michaelneas <michael.r.neas@gmail.com>
2025-06-28 21:39:37 -0400

    2161 partition array with rules

---
+++ challenges/1337/2161partitionArray.swift
@@ -0,0 +1,26 @@
+/// You are given a 0-indexed integer array nums and an integer pivot. Rearrange nums such that the following conditions are satisfied:
+/// Every element less than pivot appears before every element greater than pivot.
+/// Every element equal to pivot appears in between the elements less than and greater than pivot.
+/// The relative order of the elements less than pivot and the elements greater than pivot is maintained.
+/// More formally, consider every pi, pj where pi is the new position of the ith element and pj is the new position of the jth element. If i < j and both elements are smaller (or larger) than pivot, then pi < pj.
+/// Return nums after the rearrangement.
+
+class Solution {
+    func pivotArray(_ nums: [Int], _ pivot: Int) -> [Int] {
+        var left = [Int]()
+        var right = [Int]()
+        var mid = [Int]()
+
+        for num in nums {
+            if num > pivot {
+                right.append(num)
+            } else if num < pivot {
+                left.append(num)
+            } else {
+                mid.append(num)
+            }
+        }
+
+        return left + mid + right
+    }
+}
